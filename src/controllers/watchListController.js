fe1a4825b4ad80fbd8b07db72744d7b94e7da992
Adrian Anicete <anicete.ian@gmail.com>
2026-03-04 22:02:27 +0800

    feat: add watchlist CRUD endpoints with auth middleware

--- src/controllers/watchListController.js
+++ src/controllers/watchListController.js
@@ -1,7 +1,9 @@
-import { prisma } from "../config/db.js";
+import prisma from "../config/db.js";
+
+console.log(Object.keys(prisma));
 
 const addToWatchList = async (req, res) => {
-  const { movieId, status, rating, notes, userId } = req.body;
+  const { movieId, status, rating, notes } = req.body;
 
   const movie = await prisma.movie.findUnique({
     where: { id: movieId },
@@ -15,24 +17,24 @@ const addToWatchList = async (req, res) => {
   }
 
   // Check if already added
-  const existingInWatchList = await prisma.watchlistItem.findUnique({
+  const existingInWatchList = await prisma.watchListItem .findUnique({
     where: {
       userId_movieId: {
-        userId: userId,
+        userId: req.user.id,
         movieId: movieId
       }
     },
   });
 
-  if (!existingInWatchList) {
-    res.status(400).json({
+  if (existingInWatchList) {
+    return res.status(400).json({
       error: "Movie already in th watchlist",
     });
   }
 
-  const watchlistItem = await prisma.watchlistItem.create({
+  const watchlistItem = await prisma.watchListItem.create({
     data: {
-        userId,
+        userId: req.user.id,
         movieId,
         status: status || 'PLANNED',
         rating,
@@ -48,4 +50,72 @@ const addToWatchList = async (req, res) => {
   })
 };
 
-export { addToWatchList };
+// Update
+const updateWatchlistItem = async (req, res) => {
+  const { status, rating, notes } = req.body;
+
+  // Find watchlist item and verify ownership
+  const watchlistItem = await prisma.watchListItem.findUnique({
+    where: {id: req.params.id},
+  });
+
+  if(!watchlistItem) {
+    return res.status(404).json({error: "Watchlist item not found"});
+  };
+
+  // Ensure only owner can update
+  if(watchlistItem.userId !== req.user.id){
+    return res.status(403).json({
+      error: "Not allowed to update this watchlist"
+    });
+  };
+
+  // Build update data
+  const updateData = {};
+  if(status !== undefined) updateData.status = status.toUpperCase();
+  if(rating !== undefined) updateData.rating = rating;
+  if(notes !== undefined) updateData.notes = notes;
+
+  // Update watchlist item
+  const updateItem = await prisma.watchListItem.update({
+    where: { id: req.params.id},
+    data: { updateData },
+  });
+
+  res.status(200).json({
+  status: "Success",
+  data: {
+    watchlistItem: updateItem,
+  },
+});
+};
+
+
+
+
+// Delete
+const removeFromWatchlist = async (req, res) => {
+  const watchlistItem = await prisma.watchListItem.findUnique({
+    where: {id: req.params.id},
+  });
+
+  if(!watchlistItem) {
+    res.status(404).json({error: 'Watchlist item not found'});
+  };
+
+  // ensure owner can delete
+  if(watchlistItem.userId !== req.user.id){
+    return res.status(403).json({error: "Not allowed to update this watchlist item"})
+  };
+
+  await prisma.watchListItem.delete({
+    where: { id: req.params.id}
+  });
+
+  res.status(200).json({
+    status: "Success",
+    message: "Movie removed from watchlist"
+  });
+}
+
+export { addToWatchList, removeFromWatchlist, updateWatchlistItem };
--- src/routes/watchListRoutes.js
+++ src/routes/watchListRoutes.js
@@ -1,8 +1,21 @@
 import express from 'express';
-import { addToWatchList } from '../controllers/watchListController.js';
+import { addToWatchList, removeFromWatchlist, updateWatchlistItem } from '../controllers/watchListController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', addToWatchList)
+router.use(authMiddleware);
+
+// @desc Create/Add new movie to watchlist
+// @route {{BaseURL}}/watchlist
+router.post('/', addToWatchList);
+
+// @desc Update movie to watchlist
+// @route {{BaseURL}}/watchlist/:id
+router.put('/:id', updateWatchlistItem)
+
+// @desc Delete movie to watchlist
+// @route {{BaseURL}}/watchlist/:id
+router.delete('/:id', removeFromWatchlist);
 
 export default router;
